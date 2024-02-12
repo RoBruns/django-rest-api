7aaa3e880779b667c4ff40ca5addb3f8eca090e7
RoBruns <rodrigobrunsfh@gmail.com>
2024-02-12 07:46:55 -0300

    feat: create serializer

---
+++ school/serializer.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+from .models import Course, Student
+
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = '__all__'
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = '__all__'
