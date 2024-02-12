e02452e48a4559c075a0bec14e96caafaabcd538
RoBruns <rodrigobrunsfh@gmail.com>
2024-02-12 07:54:10 -0300

    feat: create views

--- school/views.py
+++ school/views.py
@@ -1,8 +1,20 @@
-from django.http import JsonResponse
+from rest_framework import viewsets
+
+from .models import Course, Student
+from .serializer import CourseSerializer, StudentSerializer
 
 
-def students(request):
-    if request.method == 'GET':
-        student = {'id': 1, 'name': 'Rodrigo', 'age': 20}
+class StudentViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows students to be viewed or edited.
+    """
+    queryset = Student.objects.all().order_by('id')
+    serializer_class = StudentSerializer
 
-        return JsonResponse(student)
+
+class CourseViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows courses to be viewed or edited.
+    """
+    queryset = Course.objects.all().order_by('id')
+    serializer_class = CourseSerializer
